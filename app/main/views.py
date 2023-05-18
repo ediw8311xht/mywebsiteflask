6607a0e18201cffc9f7c03cd54587a1f6fc2359f
ediw8311xht <maxballard333@gmail.com>
2023-05-18 00:35:08 -0400

    Z

--- app/main/views.py
+++ app/main/views.py
@@ -7,7 +7,8 @@ from jinja2.exceptions import TemplateNotFound
 #from .. import db
 #import psycopg2
 
-valid_pages = {'Coding', 'Writing', 'Games', 'Home'}
+valid_pages     = {'Coding', 'Writing', 'Games', 'Home'}
+has_subsections = {'Coding'}
 
 @main.route("/favicon.ico")
 def favicon_icon():
@@ -21,8 +22,13 @@ def home():
 def parent_page(pname):
     composed = f'html/{pname}.html'
     if pname in valid_pages and get_path_template(composed):
-        dirs = os.listdir(f'/{current_app.static_folder}/sub/{pname}/')
-        return render_template(composed, dirs=dirs)
+        sub_path = f'/{current_app.static_folder}/sub/{pname}'
+        sub_contents = os.listdir(f'/{sub_path}')
+        if pname in has_subsections:
+            sections = [ { 'name': x, 'dirs': os.listdir(f'/{sub_path}/{x}') } for x in sub_contents ]
+            return render_template(composed, sections=sections)
+        else:
+            return render_template(composed, dirs=sub_contents)
     abort(404)
 
 @main.route("/sub/<string:pname>/<string:cname>")
@@ -31,6 +37,11 @@ def child_page(pname, cname):
         return render_template( f'html/sub/{pname}_sub.html', url_pass=url_for('static', filename=f"sub/{pname}/{cname}/index.html") )
     abort(404)
 
+@main.route("/sub/<string:pname>/<string:p2name>/<string:cname>")
+def sub_child_page(pname, p2name, cname):
+    if pname in valid_pages and pname in has_subsections and only_alpha(cname + p2name):
+        return render_template( f'html/sub/{pname}_sub.html', url_pass=url_for('static', filename=f"sub/{pname}/{p2name}/{cname}/index.html") )
+    abort(404)
 
 #@main.route("/<str:category>/<str:path_str>")
 #def generic_file_category(category):
